b273260b32e4443faec7fbaf4826b8a8c66443f5
Roman Gęsiarz <roman@gesiarz.pl>
2020-07-29 22:32:13 +0200

    initial commit

---
+++ src/app/containers/pwa/components/pwa/pwa.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pwa',
+  templateUrl: './pwa.component.html',
+  styleUrls: ['./pwa.component.css']
+})
+export class PwaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
